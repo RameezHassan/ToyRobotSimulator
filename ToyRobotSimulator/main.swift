610a8f36d9e9c7c1e2549a062e7b783111a34fee
Rameez Hassan <rameez@abacus.co>
2020-09-19 22:56:01 +1000

    Setted up git ignore properly

---
+++ ToyRobotSimulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ToyRobotSimulator
+//
+//  Created by Rameez Hassan on 8/9/20.
+//  Copyright © 2020 Rameez Hassan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
