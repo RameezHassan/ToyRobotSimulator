e28b746bec8e51a0afec9008e35fdc4e094108e3
Rameez Hassan <rameez@abacus.co>
2020-09-22 13:29:58 +1000

    Update ToyRobotSimulatorTests.swift
    Added Test For Command Reader

--- ToyRobotSimulatorTests/ToyRobotSimulatorTests.swift
+++ ToyRobotSimulatorTests/ToyRobotSimulatorTests.swift
@@ -17,4 +17,7 @@ class ToyRobotSimulatorTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    func testCommandReader() throws {
+        RobotCommandReader().startRedingCommands()
+    }
 }
